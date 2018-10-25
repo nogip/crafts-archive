562e907e6f130bf0e5b8c8b132cf5ed568afef15
Karim Rinatovich <mtkn@tutanota.com>
2018-10-25 19:59:48 +0300

    add moving templates

--- utils/django/change_paths.py
+++ utils/django/change_paths.py
@@ -1,5 +1,38 @@
+import re, os, shutil
+
+
+def move_templates(apps, base_dir):
+    if not os.path.exists(base_dir):
+        return False
+
+    root_template_dir = os.path.join(base_dir, 'templates')
+
+    if not os.path.exists(root_template_dir):
+        return False
+
+    for t_appname in os.listdir(root_template_dir):
+
+        if t_appname not in apps:
+            return False
+
+        t_appname = os.path.join(root_template_dir, t_appname)
+        if not os.path.exists(t_appname) or not os.path.isdir(t_appname):
+            return False
+
+        app_template_dir = os.path.join(base_dir, t_appname, 'templates')
+        if not os.path.exists(app_template_dir):
+            os.mkdir(app_template_dir)
+
+        for template in os.listdir(t_appname):
+            template_old_path = os.path.join(root_template_dir, t_appname, template)
+            template_new_path = os.path.join(app_template_dir, template)
+            if template.endswith('.html'):
+                status = shutil.move(template_old_path, template_new_path)
+                print('[+] Moving {} :: {}'.format(status, template_new_path))
+
+
 def change_paths(apps, base_dir):
-    import re, os
+    move_templates(apps, base_dir)
     codec = "utf-8"
     read_t = 'r'
     write_t = 'w'
